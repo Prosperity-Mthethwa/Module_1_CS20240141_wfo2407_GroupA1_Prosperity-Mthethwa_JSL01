aa9ebf528c2b2f50f6747ce5bc417d1e7280e537
Prosperity-Mthethwa <nyamboseprosperity@gmail.com>
2024-08-29 15:24:25 +0200

    modified: script.js

--- script.js
+++ script.js
@@ -1,31 +1,12 @@
 function validateSyntax() {
     let input = document.getElementById('petInput').value;
-    let result = ''; 
-    
-    if (input.startsWith('pet_')) {
-        // Check if the characters after "pet_" are all digits (year)
-        let year = input.substring(4, 8);
-        if (!isNaN(year)) {
-            // Check if the characters after the year are all letters
-            let petName = input.substring(8);
-            let containsOnlyLetters = true;
-            for (let i = 0; i < petName.length; i++) {
-                if (!isLetter(petName[i])) {
-                    containsOnlyLetters = false;
-                    break;
-                }
-            }
-            if (containsOnlyLetters) {
-                result = "Valid Syntax";
-            } else {
-                result = "Invalid Syntax";
-            }
-        } else {
-            result = "Invalid Syntax";
-        }
-    } else {
-        result = "Invalid Syntax";
+    let num = /^\d{4}$/g;
+    let char = /[^a-zA-Z]/g;
+
+    let result = document.getElementById('result'); 
+
+    if (input.startsWith('pet_') && num && char) {
+        result.innerHTML = "Vaid Syntax 🟢"
     }
+    else {result.innerHTML = "Invalid syntax 🔴"}
 }
-
-
