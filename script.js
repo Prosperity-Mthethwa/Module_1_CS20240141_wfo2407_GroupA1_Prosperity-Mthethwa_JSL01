94896a099a1e33bffcbaa933a6d3230508b2ebac
Prosperity-Mthethwa <nyamboseprosperity@gmail.com>
2024-08-30 12:38:44 +0200

    final debug

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 function validateSyntax() {
     let input = document.getElementById('petInput').value;
-    
-
+    let format = /^pet_\d{4}[a-zA-Z]+$/g;
     let result = document.getElementById('result'); 
 
-    if (input.startsWith('pet_')) {
+    if (input.match(format)) {
         result.innerHTML = "Vaid Syntax 🟢"
     }
     else {
